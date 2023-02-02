95289af44d1d9714103ad86de98ac26e1d0c99c5
Anton Soldatov <turbolet85@gmail.com>
2023-02-02 21:53:44 +0100

    setup query object

--- controllers/products.js
+++ controllers/products.js
@@ -1,10 +1,19 @@
+const Product = require('../models/product');
+
 const getAllProductsStatic = async (req, res) => {
-	throw new Error('testing async errors');
-	res.status(200).json({ msg: 'products testing route' });
+	const products = await Product.find({ name: 'albany table' });
+	res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-	res.status(200).json({ msg: 'products route' });
+	const { featured } = req.query;
+	const queryObject = {};
+
+	if (featured) {
+		queryObject.featured = featured === 'true' ? 'true' : 'false';
+	}
+	const products = await Product.find(queryObject);
+	res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProductsStatic, getAllProducts };
